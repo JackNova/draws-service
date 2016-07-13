6f6b2ebde03f1606989984422c3be9401735eed0
Dario Iacampo <dario@jntstudio.net>
2016-07-13 07:54:51 +0200

    add disambiguate yaml library

--- scripts/ambiguous_libraries.sh
+++ scripts/ambiguous_libraries.sh
@@ -1,1 +1,3 @@
-export PYTHONPATH=$(pwd)/google_appengine/lib/webapp2-2.5.2:$(pwd)/google_appengine/lib/webob-1.2.3:$PYTHONPATH
+export PYTHONPATH=$(pwd)/google_appengine/lib/webapp2-2.5.2/:$PYTHONPATH
+export PYTHONPATH=$(pwd)/google_appengine/lib/webob-1.2.3/:$PYTHONPATH
+export PYTHONPATH=$(pwd)/google_appengine/lib/yaml-3.10/:$PYTHONPATH
