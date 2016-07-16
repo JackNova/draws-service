b775949be0ef4b8e5bd72749babf1edd2cc1d8c8
Dario Iacampo <dario@jntstudio.net>
2016-07-16 07:55:48 +0200

    fix tests: exit with code 1 if tests fail

--- test_runner.py
+++ test_runner.py
@@ -69,7 +69,8 @@ def main(sdk_path, test_path):
 
     # Discover and run tests.
     suite = unittest.loader.TestLoader().discover(test_path)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(0 if result.wasSuccessful() else 1)
 
 
 if __name__ == '__main__':
